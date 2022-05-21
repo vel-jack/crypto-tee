7993aeeda706da0e5a2211f749febc6c6e51cc00
Velmurugan R <cenavel2@gmail.com>
2022-05-21 09:39:38 +0530

    favicon added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,15 +6,17 @@ const Footer = () => {
       <div className="flex gap-2 sm:items-end sm:flex-row flex-col">
         <div className="font-bold text-lg ">Crypto-T</div>
         <div className="italic">
-          <a href="#">GitHub</a>
+          <a href="https://github.com/vel-jack/crypto-tee">GitHub</a>
         </div>
       </div>
       <div className="flex gap-2 sm:flex-row flex-col">
         <div className="italic">
-          <a href="#">Twitter,</a>
+          <a href="https://twitter.com/the_vjack" target="_blank">
+            Twitter,
+          </a>
         </div>
         <div className="italic">
-          <a href="#">Email</a>
+          <a href="mailto://cenavel2@gmail.com">Email</a>
         </div>
       </div>
     </div>
